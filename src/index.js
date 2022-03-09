5780a0d6e79a743c41fa543866d358bb361aa5f5
Aidan Loughran <86127892+aid-lo@users.noreply.github.com>
2022-03-09 13:16:35 +0000

    Rewrite

--- src/index.js
+++ src/index.js
@@ -1,169 +1,236 @@
-FormMan = () => {
+/**
+ * Stores references to form management API objects.
+ * @type Object<Object<FormManagementAPI>>
+ */
+const formAPI = {
 
 	/**
-	 * @callback InputValidateCallback A callback used to validate an input before submitting a form.
-	 * @param {HTMLInputElement} input The input element.
+	 * Form management APIs by the form element's name attribute.
+	 * @type Object<FormManagementAPI>
 	 */
+	byName: {},
 
 	/**
-	 * Managed collection of Forms
-	 * @type {{Form}}
+	 * Form management APIs by the form element's ID attribute.
+	 * @type Object<FormManagementAPI>
 	 */
-	const forms = {};
-
-	/**
-	 * Class for managing a form with FormMan.
-	 */
-	class Form {
-		/**
-		 * Validation callback functions used to validate the form's inputs.
-		 * @type {{InputValidateCallback}}
-		 * @private
-		 */
-		_inputs = {};
-
-		/**
-		 * Set up an instance from a Form element.
-		 * @param {HTMLFormElement} form
-		 */
-		constructor(form) {
-			forms[form.name] = this;
-			this._element = form;
-			this.activate();
-		};
-
-		/**
-		 * Set the function fired on submit.
-		 * @param {function} fn
-		 */
-		setSubmitFunction(fn) {
-			if (this._submitFn)
-				this._element.removeEventListener('submit', this._submitFn);
-			this._element.addEventListener('submit', fn);
-			this._submitFn = fn;
-		};
-
-		/**
-		 * Set the form to use FormMan for validating and POSTing the form using fetch.
-		 */
-		activate() {
-			this.setSubmitFunction(e => {
-				e.preventDefault();
-				const d = new URLSearchParams();
-				let error = false;
-				for (const i of this._element.getElementsByTagName('input')) if (i.type !== 'submit') {
-					if (!error && this._inputs[i.name] && !this._inputs[i.name](i)) {
-						i.focus();
-						error = true;
-					}
-					else d.append(i.name, i.value);
-				}
-				if (error) return;
-				fetch(this._element.action, {
-					method: "POST",
-					redirect: "follow",
-					headers: {'X-Doji-Fetch': 'true'},
-					body: d
-				}).then(r => {
-					if (r.redirected) {
-						window.location.href = r.url;
-						return {then: () => {}};
-					}
-					return r;
-				}).then(r => r.text())
-				.then(d => console.log(d));
-			});
-		};
-
-		/**
-		 * Disable the form. Submit button will do nothing.
-		 */
-		disable() {
-			this.setSubmitFunction(e => e.preventDefault());
-		};
-
-		/**
-		 * Registers a callback that will fire when a form is submitted to verify the content of an input. If a form has at
-		 * least one input validation callback return false, then the data will not be POSTed.
-		 * @param {string} input The name attribute of an input element of the form.
-		 * @param {InputValidateCallback} callback The function used to validate an input before submitting the form. The
-		 * input element is passed through.
-		 */
-		registerInputValidation(input, callback) {
-			this._inputs[input] = callback;
-		};
-
-		/**
-		 * Removes the callback associated with an input.
-		 * @param {string} input The name attribute of an input element of the form.
-		 */
-		removeInputValidation(input) {
-			this._inputs[input] = null;
-		};
-	}
-
-	/**
-	 * Set up an individual form in the document.
-	 * @param {HTMLFormElement} form The form element.
-	 */
-	formMan.initForm = form => {
-		if (!forms[form.name])
-			new Form(form);
-	};
-
-	/**
-	 * Set up all uninitialised forms in the document.
-	 */
-	formMan.initAllForms = () => {
-		for (const f of document.forms)
-			formMan.initForm(f);
-	};
-
-	/**
-	 * Set the form to use FormMan for validating and submitting the form. Form will submit to the URL in the "action"
-	 * attribute using the fetch API. The data will be sent as URL encoded data using URLSearchParams().
-	 * @param {string} form The name attribute of an initialised form.
-	 */
-	formMan.activate = form => {
-		const f = forms[form];
-		if (!f) return;
-		f.activate();
-	};
-
-	/**
-	 * Disables the form. Note that nothing will happen when the submit button is pressed.
-	 * @param {string} form The name attribute of an initialised form.
-	 */
-	formMan.disable = form => {
-		const f = forms[form];
-		if (!f) return;
-		f.disable();
-	};
-
-	/**
-	 * Registers a callback that will fire when a form is submitted to verify the content of an input. If a form has at
-	 * least one input validation callback return false, then the data will not be POSTed.
-	 * @param {string} form The name attribute of a form element.
-	 * @param {string} input The name attribute of an input element of the form.
-	 * @param {InputValidateCallback} callback The function used to validate an input before submitting the form. The
-	 * input element is passed through.
-	 */
-	formMan.registerInputValidation = (form, input, callback) => {
-		const f = forms[form];
-		if (!f) return;
-		f.registerInputValidation(input, callback);
-	};
-
-	/**
-	 * Removes the callback associated with an input.
-	 * @param {string} form The name attribute of a form element.
-	 * @param {string} input The name attribute of an input element of the form.
-	 */
-	formMan.removeInputValidation = (form, input) => {
-		const f = forms[form];
-		if (!f) return;
-		f.removeInputValidation(input);
-	};
-
-	return formMan;
+	byID: {}
 };
+
+/**
+ * Finds any unmanaged forms and initialises them with this module. This happens when the module is first loaded, but
+ * can be re-called if forms are added later on. If only one form needs to be added, use `manageForm` instead.
+ * @return void
+ */
+export function loadAllForms() {
+	for (const formElement of document.forms)
+		manageForm(formElement);
+}
+
+// Initialise all forms when the module is loaded
+loadAllForms();
+
+/**
+ * Gets the form management API for a form. Attempts to initialise the form if it is new, but otherwise returns false
+ * if the form does not exist.
+ * @param {HTMLFormElement | string} form A form element or its name or ID attribute.
+ * @return {FormManagementAPI | false}
+ */
+export function manageForm(form= '') {
+	switch (typeof form) {
+		case "string":
+			return formAPI.byName[form] ?? formAPI.byID[form] ?? (() => {
+				for (const formElement of document.forms)
+					if (formElement.name === form)
+						return formAPI.byName[formElement.name] = formAPI.byID[formElement.id] = new FormManagementAPI(formElement);
+				for (const formElement of document.forms) {
+					if (formElement.id === form)
+						return formAPI.byName[formElement.name] = formAPI.byID[formElement.id] = new FormManagementAPI(formElement);
+				}
+				return false;
+			})();
+		case "object":
+			return formAPI.byName[form.name] ?? formAPI.byID[form.id] ?? (formAPI.byName[form.name] = formAPI.byID[form.id] = new FormManagementAPI(form));
+	}
+}
+
+/**
+ * A module for managing web forms. Handles async submit and validation.
+ */
+export default {
+	manageForm: manageForm,
+	loadAllForms: loadAllForms
+}
+
+/**
+ * API for managing a form.
+ * @param {HTMLFormElement} formElement
+ * @constructor
+ */
+function FormManagementAPI(formElement) {
+
+	// TODO: add support for input validation functions that work via a promise or callback
+
+	/**
+	 * A callback that is used to test validation of an input when a form is submitted.
+	 * @callback InputValidationFunction
+	 * @param {HTMLInputElement} input
+	 * @return boolean
+	 */
+
+	/**
+	 * Validation functions for inputs within this form.
+	 * @type Object<InputValidationFunction>
+	 */
+	const inputs = {};
+
+	/**
+	 * A callback for handling fetch response.
+	 * @callback FetchPromiseCallback
+	 * @param {Promise<Response>} response
+	 */
+
+	/**
+	 * Placeholder for a fetch callback that the user can override.
+	 * @param {Promise<Response>} response
+	 */
+	let fetchCallback = response => {};
+
+	/**
+	 * User overridden fetch parameters.
+	 * @type RequestInit
+	 */
+	let fetchInit = {};
+
+	/**
+	 * This form's submit event handler.
+	 * @param {SubmitEvent} event
+	 * @return void
+	 */
+	const onSubmit = event => {
+
+		// Prevent normal submit behaviour
+		event.preventDefault();
+
+		// Request is sent URL encoded
+		// TODO: add support for other methods
+		const params = new URLSearchParams();
+
+		// Cycle through inputs, validating them
+		// Error is set to true if validation fails
+		let error = false;
+		for (const input of formElement.getElementsByTagName('input')) {
+			// Only process inputs that aren't the submit button
+			if (input.type !== 'submit') {
+				// If a validation function has been registered for the input, test it
+				// All are tested, even if an error is found as the user might have UX logic in them
+				// TODO: an option to abort after the first error
+				if (inputs[input.name] && !inputs[input.name](input) && !error) {
+					error = true;
+					// First input to raise validation error gets focused
+					input.focus();
+				}
+				// Continue appending data to the parameters so long as no error has raised
+				else if (!error) params.append(input.name, input.value);
+			}
+		}
+
+		// Cancel submit if there is a validation problem
+		if (error) return;
+
+		fetchCallback(fetch(formElement.action, Object.assign({
+			body: params,
+			method: formElement.method
+		}, fetchInit)));
+	};
+
+	// Manage the form's submit event by default
+	formElement.addEventListener('submit', onSubmit);
+	let isHandlingSubmit = true;
+
+	let api;
+	return api = {
+
+		/**
+		 * Enables handling the submit event for the form. (Enabled by default.)
+		 * @return FormManagementAPI
+		 */
+		enableSubmitHandling: () => {
+			if (!isHandlingSubmit) {
+				formElement.addEventListener('submit', onSubmit);
+				isHandlingSubmit = true;
+			}
+			return api;
+		},
+
+		/**
+		 * Disables handling the submit event for the form.
+		 * @return FormManagementAPI
+		 */
+		disableSubmitHandling: () => {
+			if (isHandlingSubmit) {
+				formElement.removeEventListener('submit', onSubmit);
+				isHandlingSubmit = false;
+			}
+			return api;
+		},
+
+		/**
+		 * Registers an input validation function.
+		 * Validation functions are passed the input element as a parameter, and **must return a boolean value**.
+		 *
+		 * Only one function is registered at a time. Subsequent calls for the same input will simply replace the
+		 * previously registered function.
+		 *
+		 * @param {string} inputName The name attribute of the input.
+		 * @param {InputValidationFunction} validationFn
+		 * @return FormManagementAPI
+		 */
+		registerInputValidator: (inputName, validationFn) => {
+			inputs[inputName] = validationFn;
+			return api;
+		},
+
+		/**
+		 * Removes an input validation function that was previously registered.
+		 * @param {string} inputName The name attribute of the input.
+		 * @return FormManagementAPI
+		 */
+		removeInputValidator: inputName => {
+			delete inputs[inputName];
+			return api;
+		},
+
+		/**
+		 * Set the callback that handles the fetch response promise upon a successful submit event.
+		 * @param {FetchPromiseCallback} callback A promise from the fetch API.
+		 * @return FormManagementAPI
+		 */
+		setSubmitResponseCallback: callback => {
+			fetchCallback = callback;
+			return api;
+		},
+
+		/**
+		 * Override parameters that are set in the fetch RequestInit upon a successful submit event.
+		 * @param {RequestInit} fetchParams
+		 * @return FormManagementAPI
+		 */
+		setFetchParams: fetchParams => {
+			console.log(api);
+			fetchInit = fetchParams;
+			return api;
+		}
+	};
+}
+
+/**
+ * API for managing a form. All functions return the API, so function calls can be chained together in one statement.
+ * @typedef FormManagementAPI
+ * @property {function: FormManagementAPI} enableSubmitHandling Enables handling the submit event for the form. (Enabled by default.)
+ * @property {function: FormManagementAPI} disableSubmitHandling Disables handling the submit event for the form.
+ * @property {function(inputName: string): FormManagementAPI} registerInputValidator Registers an input validation function. Validation functions are passed the input element as a parameter, and **must return a boolean value**. Only one function is registered at a time. Subsequent calls for the same input will simply replace the previously registered function.
+ * @property {function(inputName: string): FormManagementAPI} removeInputValidator Removes an input validation function that was previously registered.
+ * @property {function(callback: FetchPromiseCallback): FormManagementAPI} setSubmitResponseCallback Set the callback that handles the fetch response promise upon a successful submit event.
+ * @property {function(fetchParams: RequestInit): FormManagementAPI} setFetchParams Override parameters that are set in the fetch RequestInit upon a successful submit event.
+ */
